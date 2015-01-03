f2848e7afcd555a73d0a05d431175dca8029b8a2
Akshansh986 <akshanshsingh@Akshanshs-Mac-Pro.local>
2015-01-03 23:23:57 +0530

    Not sending unidentified offline song in user activity

--- suzik/src/main/java/com/blackMonster/suzik/musicstore/userActivity/UserActivityManager.java
+++ suzik/src/main/java/com/blackMonster/suzik/musicstore/userActivity/UserActivityManager.java
@@ -3,6 +3,7 @@ package com.blackMonster.suzik.musicstore.userActivity;
 import android.content.Context;
 import static com.blackMonster.suzik.util.LogUtils.LOGD;
 
+import com.blackMonster.suzik.musicstore.MusicServerUtils;
 import com.blackMonster.suzik.musicstore.module.UserActivity;
 import com.blackMonster.suzik.sync.Syncer;
 
@@ -11,6 +12,10 @@ public class UserActivityManager {
 	
 	public static void add(UserActivity activity, Context context) {
 		LOGD(TAG,"add");
+        if (!activity.isOnlineSong() && MusicServerUtils.isDummyServerSongId(activity.songId())) {
+            LOGD(TAG,"Song id dummy, Skipped..");
+            return;
+        }
 		QueueUserActivity.insert(activity, context);
 		Syncer.batchRequest(UserActivityQueueSyncer.class, context);
 	}
