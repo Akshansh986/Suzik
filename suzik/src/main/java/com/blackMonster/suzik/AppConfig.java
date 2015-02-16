ba4eb56a47e79bf0ab1a8732a93875eb1c5660b4
Akshansh986 <akshanshsingh@Akshanshs-Mac-Pro.local>
2015-02-16 15:57:19 +0530

    beta 1.2(4) released

--- suzik/src/main/java/com/blackMonster/suzik/AppConfig.java
+++ suzik/src/main/java/com/blackMonster/suzik/AppConfig.java
@@ -1,7 +1,7 @@
 package com.blackMonster.suzik;
 
 public class AppConfig {
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 	public static final String MAIN_URL = "http://suzik.elasticbeanstalk.com/music/reciver.php";
 	public static final String FLAG_URL = "http://suzik.elasticbeanstalk.com/music/songLink/flagSong.php";
 
